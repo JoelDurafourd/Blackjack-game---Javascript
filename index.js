7b81b04614ab983c11689f7b224e9031780f0126
JoelDurafourd <joel.durafourd@gmail.com>
2024-03-22 21:51:13 +0900

    created new buttons and features, including start game scripts

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
+let messageEl = document.getElementById("message-el")
+
+let sumEl = document.getElementById("sum-el")
+
+let cardsEl = document.getElementById("cards-el")
+
 let min = 2 
 
 let max = 11
 
-let firstCard = Math.floor(Math.random() * (max - min + 1)) + min
+let hasBlackJack = false
+let isAlive = true
 
-let secondCard = Math.floor(Math.random() * (max - min + 1)) + min
+let message = ""
 
-let sum = firstCard + secondCard
 
-if (sum < 21) {
-    console.log("Do you want to draw a new card?")
-} else if (sum === 21) {
-    console.log("Blackjack!")
-} else {
-    console.log("You're out of luck, you lose!")
+function startGame() {
+    let firstCard = Math.floor(Math.random() * (max - min + 1)) + min
+    let secondCard = Math.floor(Math.random() * (max - min + 1)) + min
+    let sum = firstCard + secondCard
+        if (sum < 21) {
+            message = "Do you want to draw a new card?"
+        } else if (sum === 21) {
+            message = "Blackjack!"
+            hasBlackJack = true
+        } else {
+            message = "You're out of luck, you lose!"
+            isAlive = false
+        }
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    sumEl.textContent = "Sum: " + sum
+    messageEl.textContent = message
 }
+
